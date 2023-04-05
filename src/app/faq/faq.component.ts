ab5ca61f2b1fa55127f6fda0b0e3f4bb033ddfd3
Robert Willemelis <willi84@users.noreply.github.com>
2023-04-05 12:48:47 +0200

    fix: brackground path

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
-  faqImage = '../../../../assets/backgrounds/djangogirls.jpg';
+  faqImage = '../../../../assets/backgrounds/djangogirls/djangogirls-xl.webp';
   constructor() { }
 
   ngOnInit(): void {

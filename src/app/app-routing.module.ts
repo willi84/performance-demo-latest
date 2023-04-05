764f0d023ff784dc298c890126530c51f9aa3912
Robert Willemelis <robert.dev@gmx.de>
2023-04-05 02:37:48 +0200

    chore: udpate to angualr 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,10 +15,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    relativeLinkResolution: 'legacy',
     scrollPositionRestoration: 'enabled',
     anchorScrolling: 'enabled'
-  })],
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
